ce43a5acbc97c661d8c6c91cad3ae70744fc641d
bamtolee <79846677+bamtolee@users.noreply.github.com>
2022-11-05 21:43:58 +0900

    2675

---
+++ C++_ex/2675_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int r;
+        string s;
+        cin >> r >> s;
+        for(unsigned long int j = 0; j < s.length(); j++)
+        {
+            for(int k = 0; k < r; k++)
+                cout << s.at(j);
+        }
+        cout << "\n";
+    }
+    return 0;
+}
