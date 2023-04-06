f57b5be52d45728199fa635dd37e01cadd4eb58b
bamtolee <79846677+bamtolee@users.noreply.github.com>
2023-04-07 00:41:45 +0900

    230407 11401이항계수3 13977이항계수와쿼리 페르마소정리

---
+++ C++_gold/11401_g.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+typedef long long int ll;
+ll mod = 1000000007;
+ll factorial[4000001];
+
+ll dqPow(ll a, ll b)
+{
+    ll i = 1;
+    while(b > 0)
+    {
+        if((b & 1) == 1)
+        {
+            i *= a;
+            i %= mod;
+        }
+        a *= a;
+        a %= mod;
+        b = b >> 1;
+    }
+    return i;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n, k;
+    cin >> n >> k;
+
+    factorial[0] = 1;
+    factorial[1] = 1;
+    for(int i = 2; i <= n; i++)
+        factorial[i] = factorial[i - 1] * (ll)i % mod;
+
+    ll numer = factorial[n];
+    ll denom = dqPow(factorial[k], mod - 2);
+    denom *= dqPow(factorial[n - k], mod - 2);
+    denom %= mod;
+    ll ret = numer * denom % mod;
+    cout << ret;
+    return 0;
+}
